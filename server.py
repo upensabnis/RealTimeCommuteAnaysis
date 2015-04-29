650a26e07d57b9ecc39bbd4776d364632bde61be
crdsrm <rach9569@colorado.edu>
2015-04-29 12:18:07 -0600

    python server

--- server.py
+++ server.py
@@ -12,10 +12,6 @@ Mongo_Host = 'localhost'
 Mongo_Port = 27017
 Dbs_Name = 'twitter'
 collection_name = 'test2'
-FIELDS = {'firstName':True,'lastName':True}
-
-
-
 
 @app.route('/')
 def index():
@@ -27,14 +23,12 @@ def Data():
 	client = MongoClient("localhost", 27017)
 	db = client.test_database
 	collection = db.posts
-	data1 = []
 	json_data=[]
 	data = collection.find()	
 	for record in data:
-		data1.append(record)
+		json_data.append(record)
 	json_data = json.dumps(data1,default= json_util.default)
 	return json_data
-
 
 if __name__ == '__main__':
 	app.debug = True
