246e266cf5d0ffd3bc18708d8f358ade1085b859
crdsrm <rach9569@colorado.edu>
2015-04-30 02:29:16 -0600

    Complete End to End working of D3

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import url_for
 from pymongo import MongoClient
 import json
 from bson import json_util
@@ -7,21 +8,15 @@ from bson.json_util import dumps
 
 app = Flask(__name__)
 
-
-Mongo_Host = 'localhost'
-Mongo_Port = 27017
-Dbs_Name = 'twitter'
-collection_name = 'test2'
-
 @app.route('/')
 def index():
-	return render_template("index.html");
+	return render_template("Data.html");
 	
 #This will retreive the data from the server and Mongodb and store it in to list. 
 @app.route('/data')
-def Data():
+def data():
 	client = MongoClient("localhost", 27017)
-	db = client.test_database
+	db = client.student
 	collection = db.posts
 	json_data=[]
 	data = collection.find()	
@@ -29,7 +24,7 @@ def Data():
 		json_data.append(record)
 	json_data = json.dumps(json_data,default= json_util.default)
 	return json_data
-
+	
 if __name__ == '__main__':
 	app.debug = True
 	app.run()
